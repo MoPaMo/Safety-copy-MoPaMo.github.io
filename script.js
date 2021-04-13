d86358a8bf0b8ab51845b7a4951d4301b557a99c
MoPaMo <mpm1301@web.de>
2021-04-13 18:53:05 +0200

    add data loading

--- script.js
+++ script.js
@@ -1,36 +1,39 @@
-document.addEventListener('DOMContentLoaded', () => {
-
+document.addEventListener("DOMContentLoaded", () => {
   // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
 
   // Check if there are any navbar burgers
   if ($navbarBurgers.length > 0) {
-
     // Add a click event on each of them
-    $navbarBurgers.forEach( el => {
-      el.addEventListener('click', () => {
-
+    $navbarBurgers.forEach((el) => {
+      el.addEventListener("click", () => {
         // Get the target from the "data-target" attribute
         const target = el.dataset.target;
         const $target = document.getElementById(target);
 
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-
+        el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
       });
     });
   }
   //https://api.github.com/users/MoPaMo/events
-  fetch('https://api.github.com/users/MoPaMo/events', {
-        method: 'get'
+  fetch("https://api.github.com/users/MoPaMo/events", {
+    method: "get",
+  })
+    .then((response) => response.json())
+    .then((jsonData) => {
+      console.log(jsonData);
+      let data = jsonData.filter((a) => a.type == "PushEvent");
+      let parent = document.getElementById("col"); //.innerHTML=data
+      for (let i = 0; i < 2; i++) {
+        parent.children[i].innerHTML = data[i].payload.commits[0].message;
+      }
     })
-    .then(response => response.json())
-    .then(jsonData => {console.log(jsonData)
-    let data = jsonData.filter((a)=>a.type=="PushEvent")
-    document.getElementById("name").innerHTML=data
-    })
-    .catch(err => {
-            //error block
-        })
+    .catch((err) => {
+      //error block
+    });
 });
