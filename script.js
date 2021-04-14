cdd974d27d2fefebd19f249380537d70f41009ef
MoPaMo <mpm1301@web.de>
2021-04-14 15:18:02 +0200

    add responsive font variants

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", () => {
       for (let i = 0; i < 3; i++) {
         let b = data[i].payload.commits[0],
           c = parent.children[i].children[0],
-          a =`<span class="is-size-4">${b.message}</span>`;
+          a =`<span class="is-size-6 is-size-5-widescreen">${b.message}</span>`;
           console.log(a)
         c.innerHTML = a.toString();
       }
